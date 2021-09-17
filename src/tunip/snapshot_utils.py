d9e9e23e6b9790e4c1aac50f03bc2294a21dcee2
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2021-09-17 16:41:13 +0900

    [Python] fix: pass config to get file handler

--- src/tunip/snapshot_utils.py
+++ src/tunip/snapshot_utils.py
@@ -5,7 +5,7 @@ from typing import Optional
 from tunip.path_utils import NautsPath
 from tunip.service_config import ServiceLevelConfig
 
-import tunip.file_utils
+import tunip.file_utils as file_utils
 
 
 def snapshot_now():
@@ -16,6 +16,7 @@ def snapshot_now():
 def snapshot2datetime(snapshot):
     dt = datetime.strptime(snapshot, r"%Y%m%d_%H%M%S_%f")
     return dt
+
 
 # snapshot = snapshot_now()
 # dt = snapshot2datetime(snapshot)
@@ -35,14 +36,16 @@ class NotSupportSnapshotException(Exception):
 class NoSnapshotPathException(Exception):
     pass
 
-class SnapshotPathProvider:
 
+class SnapshotPathProvider:
     def __init__(self, service_config: ServiceLevelConfig):
         self.config = service_config
 
     def provide(self, nauts_path: NautsPath) -> Optional[list]:
         snapshot_paths = None
-        file_handler = file_utils.services.get(self.config.filesystem.upper())
+        file_handler = file_utils.services.get(
+            self.config.filesystem.upper(), config=self.config.config
+        )
         if isinstance(nauts_path, Snapshot) and nauts_path.has_snapshot():
             snapshot_paths = file_handler.list_dir(repr(nauts_path))
         else:
