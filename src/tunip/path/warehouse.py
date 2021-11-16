afaa655090c07ca1475714706b1da3a4d694f831
hyjin-asc <hyunyoung.jin@ascentnet.co.jp>
2021-11-17 01:17:06 +0900

    [Python] add: warehouse span set path #46

--- src/tunip/path/warehouse.py
+++ src/tunip/path/warehouse.py
@@ -47,3 +47,41 @@ class WarehouseEntitySetDomainSnapshotPath(WarehouseEntitySetDomainPath):
         return WarehouseEntitySetDomainSnapshotPath(
             parent.user_name, parent.source_type, parent.domain_name, snapshot_dt
         )
+
+class WarehouseSpanSetPath(WarehousePath):
+    def __init__(self, user_name, source_type):
+        super(WarehouseSpanSetPath, self).__init__(user_name)
+        self.source_type = source_type
+        
+    def __repr__(self):
+        return f"{super().__repr__()}/span_set/{self.source_type}"
+
+
+class WarehouseSpanSetDomainPath(WarehouseSpanSetPath):
+    def __init__(self, user_name, source_type, domain_name):
+        super(WarehouseSpanSetDomainPath, self).__init__(user_name, source_type)
+        self.domain_name = domain_name
+
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.domain_name}"
+
+    def has_snapshot(self):
+        return True
+
+
+class WarehouseSpanSetDomainSnapshotPath(WarehouseSpanSetDomainPath):
+    def __init__(self, user_name, source_type, domain_name, snapshot_dt):
+        super(WarehouseSpanSetDomainSnapshotPath, self).__init__(user_name, source_type, domain_name)
+        self.snapshot_dt = snapshot_dt
+
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.snapshot_dt}"
+    
+    def has_snapshot(self):
+        return False
+
+    @classmethod
+    def from_parent(cls, parent: WarehouseSpanSetDomainPath, snapshot_dt: str):
+        return WarehouseSpanSetDomainSnapshotPath(
+            parent.user_name, parent.source_type, parent.domain_name, snapshot_dt
+        )
