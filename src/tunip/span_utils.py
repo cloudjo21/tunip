7b5b26407e91061071799dc5f7945950cba5406d
hyjin-asc <hyunyoung.jin@ascentnet.co.jp>
2021-11-18 10:29:54 +0900

    [Python] fix: span record and add mention record #46

--- src/tunip/span_utils.py
+++ src/tunip/span_utils.py
@@ -3,30 +3,54 @@ from tunip.hash_utils import hash_func
 
 @dataclass
 class SpanRecord:
-    title: str
-    spans: list
+    span: str
+    span_type: str
     hash_value: int = -1
 
     def __hash__(self):
-        return hash_func(self.title[:50])
+        return hash_func(self.span[:50])
 
     def to_json(self):
         return {
-            'title': self.title,
-            'spans': self.spans
+            'span': self.span,
+            'span_type': self.span_type
         }
 
     def update(self, other):
-        self.title = other.title
-        self.spans = other.spans
-        
-    def update_spans(self, other):
-        self.spans = other.spans
+        self.span = other.span
+        self.span_type = other.span_type
     
     @classmethod
     def from_json_entry(self, entry):
         record = SpanRecord(
-            title=entry["title"],
-            spans=entry["spans"]
+            span=entry["span"],
+            span_type=entry["span_type"]
+        )
+        return record
+    
+@dataclass
+class MentionRecord:
+    mention: str
+    title: str
+    hash_value: int = -1
+
+    def __hash__(self):
+        return hash_func(self.mention[:50])
+
+    def to_json(self):
+        return {
+            'mention': self.mention,
+            'title': self.title
+        }
+
+    def update(self, other):
+        self.mention = other.mention
+        self.title = other.title
+    
+    @classmethod
+    def from_json_entry(self, entry):
+        record = MentionRecord(
+            mention=entry["mention"],
+            title=entry["title"]
         )
         return record
