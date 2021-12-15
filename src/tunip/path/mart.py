3a098a8c145b91fe6c7e84cf555e4030d969ac2e
yhcho-asc <yhcho-asc@ascentkorea.co.jp>
2021-12-15 11:09:25 +0900

    [Python] feat: add path definitions for vector indexing #70 #71 #72 #73

--- src/tunip/path/mart.py
+++ src/tunip/path/mart.py
@@ -55,3 +55,85 @@ class MartCorpusDomainSnapshotPath(MartCorpusDomainPath):
         return MartCorpusDomainSnapshotPath(
             parent.user_name, parent.task_name, parent.domain_name, snapshot_dt
         )
+
+
+class MartVectorIndexPath(MartPath):
+    def __init__(self, user_name):
+        super(MartVectorIndexPath, self).__init__(user_name)
+    
+    def __repr__(self):
+        return f"{super().__repr__()}/vector_index"
+
+
+class MartVectorIndexTaskNamePath(MartVectorIndexPath):
+    def __init__(self, user_name, task_name):
+        super(MartVectorIndexTaskNamePath, self).__init__(user_name)
+        self.task_name = task_name
+
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.task_name}"
+
+
+class MartVectorIndexDocumentPath(MartVectorIndexTaskNamePath):
+    def __init__(self, user_name):
+        super(MartVectorIndexDocumentPath, self).__init__(user_name, 'document')
+    
+    def __repr__(self):
+        return f"{super().__repr__()}"
+
+
+class MartVectorIndexDocumentSourcePath(MartVectorIndexDocumentPath):
+    def __init__(self, user_name, source_name):
+        super(MartVectorIndexDocumentSourcePath, self).__init__(user_name)
+        self.source_name = source_name
+    
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.source_name}"
+
+
+class MartVectorIndexDocumentSourceIndexTypePath(MartVectorIndexDocumentSourcePath):
+    def __init__(self, user_name, source_name, index_type):
+        super(MartVectorIndexDocumentSourceIndexTypePath, self).__init__(user_name, source_name)
+        self.index_type = index_type
+    
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.index_type}"
+
+    def has_snapshot(self):
+        return True
+
+
+class MartVectorIndexDocumentSourceIndexTypeSnapshotPath(MartVectorIndexDocumentSourceIndexTypePath):
+    def __init__(self, user_name, source_name, index_type, snapshot_dt):
+        super(MartVectorIndexDocumentSourceIndexTypeSnapshotPath, self).__init__(user_name, source_name, index_type)
+        self.snapshot_dt = snapshot_dt
+    
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.snapshot_dt}"
+
+    def has_snapshot(self):
+        return False
+    
+    @classmethod
+    def from_parent(cls, parent: MartVectorIndexDocumentSourceIndexTypePath, snapshot_dt: str):
+        return MartVectorIndexDocumentSourceIndexTypeSnapshotPath(
+            parent.user_name, parent.source_name, parent.index_type, snapshot_dt
+        )
+
+
+class MartPretrainedModelPath(MartPath):
+    def __init__(self, user_name, model_name):
+        super(MartPretrainedModelPath, self).__init__(user_name)
+        self.model_name = model_name
+
+    def __repr__(self):
+        return f"{super().__repr__()}/plm/models/{self.model_name}"
+
+
+class MartPretrainedModelRuntimePath(MartPretrainedModelPath):
+    def __init__(self, user_name, model_name, runtime_type):
+        super(MartPretrainedModelRuntimePath, self).__init__(user_name, model_name)
+        self.runtime_type = runtime_type
+
+    def __repr__(self):
+        return f"{super().__repr__()}/{self.runtime_type}"
