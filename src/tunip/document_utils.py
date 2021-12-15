55ec30853e5dfd90bee086dc591883d360bd91e6
hyjin-asc <hyunyoung.jin@ascentnet.co.jp>
2021-12-15 16:21:21 +0900

    [Python] add: anchor document record #74

--- src/tunip/document_utils.py
+++ src/tunip/document_utils.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, field
-from tunip.hash_utils import hash_func
+from dataclasses import dataclass
 
 @dataclass
 class DocumentRecord:
@@ -21,5 +20,37 @@ class DocumentRecord:
         record = DocumentRecord(
             title=entry["title"],
             text=entry["text"]
+        )
+        return record
+    
+    
+@dataclass
+class AnchorDocumentRecord:
+    parent_title: str
+    parent_text: str
+    anchor_titles: str
+    anchor_texts: str
+
+    def to_json(self):
+        return {
+            'parent_title': self.parent_title,
+            'parent_text': self.parent_text,
+            'anchor_titles': self.anchor_titles,
+            'anchor_texts': self.anchor_texts,
+        }
+
+    def update(self, other):
+        self.parent_title = other.parent_title
+        self.parent_text = other.parent_text
+        self.anchor_titles = other.anchor_titles
+        self.anchor_texts = other.anchor_texts
+    
+    @classmethod
+    def from_json_entry(self, entry):
+        record = AnchorDocumentRecord(
+            parent_title=entry["parent_title"],
+            parent_text=entry["parent_text"],
+            anchor_titles=entry["anchor_titles"],
+            anchor_texts=entry["anchor_texts"]
         )
         return record
