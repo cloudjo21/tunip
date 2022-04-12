2c8ad39ba42840f5875494db989d5112b990a8fc
hyjin-asc <hyunyoung.jin@ascentnet.co.jp>
2022-04-12 10:32:07 +0900

    [Python] add: matplotlib env #207

---
+++ src/tunip/matplotlib_env.py
@@ -0,0 +1,16 @@
+import matplotlib
+import warnings
+from matplotlib import font_manager, rc
+
+
+warnings.filterwarnings(action='ignore')
+
+from sys import platform
+
+if platform == "win32":
+    matplotlib.font_manager._rebuild()
+
+font_path = "/usr/share/fonts/nhn-nanum/NanumGothic.ttf"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+
