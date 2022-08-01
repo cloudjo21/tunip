2a53f2c0f59bf4370b59b0f9495f8c63592e637b
ed <cloudjo21@gmail.com>
2022-08-01 09:55:28 +0900

    fix: allow runtime configuration to set font in win32

--- src/tunip/matplotlib_env.py
+++ src/tunip/matplotlib_env.py
@@ -10,7 +10,7 @@ from sys import platform
 if platform == "win32":
     matplotlib.font_manager._rebuild()
 
-font_path = "/usr/share/fonts/nhn-nanum/NanumGothic.ttf"
-font = font_manager.FontProperties(fname=font_path).get_name()
-rc('font', family=font)
+    font_path = "/usr/share/fonts/nhn-nanum/NanumGothic.ttf"
+    font = font_manager.FontProperties(fname=font_path).get_name()
+    rc('font', family=font)
 
