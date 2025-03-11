7fdd32c91969fd59762a82792bb5f19898b27d79
cloudjo21 <cloudjo21@gmail.com>
2025-03-12 08:05:53 +0900

    [tunip-7] fix: abspath for local path builder

--- src/tunip/path_utils.py
+++ src/tunip/path_utils.py
@@ -1,3 +1,5 @@
+import os
+
 from abc import ABC, abstractmethod
 from glob import glob
 from typing import Optional, TypeVar
@@ -214,7 +216,7 @@ class LocalPathProvider(FileSystemPathProvider):
         self.local_username = config.get("local.username") or "nauts"
     
     def build(self, path, prepend_protocol=False):
-        file_path = f"{NAUTS_LOCAL_ROOT}/{path}"
+        file_path = os.path.abspath(f"{NAUTS_LOCAL_ROOT}/{path}")
         return file_path
 
 
